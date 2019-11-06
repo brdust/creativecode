2473aaf8f8f302e1e8161a1a80f3cfc105feab63
brdust <54549416+brdust@users.noreply.github.com>
2019-11-06 10:25:32 -0500

    Update Poem of Fact.py

--- poem/Poem of Fact.py
+++ poem/Poem of Fact.py
@@ -27,6 +27,7 @@ nouns = []
 for word,pos in blob.tags:
   if (pos == 'NN'):
     nouns.append(word)
+
 verbs = []
 for word,pos in blob.tags:
   if (pos == 'VB'):
@@ -37,52 +38,74 @@ for word,pos in blob.tags:
   if (pos == 'NNP'):
     nounphrase.append(word)
 
-print('Poem of Fact')
+there = []
+for word,pos, in blob.tags:
+  if (pos == 'EX'):
+    there.append(word)
+
+pronoun = []
+for word,pos in blob.tags:
+  if (pos == 'PRP'):
+    pronoun.append(word)
+
+presverb = []
+for word,pos in blob.tags:
+  if (pos == 'VBP'):
+    presverb.append(word)
+
+plnoun = []
+for word,pos in blob.tags:
+  if (pos == 'NNS'):
+    plnoun.append(word)
+
+gerund = []
+for word,pos in blob.tags:
+  if (pos == 'VBG'):
+    gerund.append(word)
+
+# modal = [" "]
+# for word,pos in blob.tags:
+#   if (pos == 'MD'):
+#     modal = []
+
+
+
     
 for i in range(1):
   a1 = random.choice(adjectives)
   a2 = random.choice(adjectives)
+  a3 = random.choice(adjectives)
+  a4 = random.choice(adjectives)
+  a5 = random.choice(adjectives)
+  a6 = random.choice(adjectives)
   n1 = random.choice(nouns)
-  np1 = random.choice(nounphrase)
-  vb = random.choice(verbs)
-  print(a1 + " " + n1 + " " +  vb  + " " + a2  + " " + np1)
-
-for i in range(1):
-  a1 = random.choice(adjectives)
-  n1 = random.choice(nouns)
-  np1 = random.choice(nounphrase)
-  vb = random.choice(verbs)
-  print(np1 + " " +  vb  + " " + a1  + " " + n1)
-
-for i in range(1):
-  print( blob.noun_phrases[random.randint(0,850)])
-  print( blob.words[random.randint(0,12787)])
-  
-for i in range(1):
-  vb = random.choice(verbs)
-  print(vb)
-
-for i in range(1):
-  a1 = random.choice(adjectives)
-  a2 = random.choice(adjectives)
-  n1 = random.choice(nouns)
-  np1 = random.choice(nounphrase)
-  vb = random.choice(verbs)
-  print(a1 + " " + n1 + " " +  vb  + " " + a2  + " " + np1)
-  
-for i in range(1):
-  a1 = random.choice(adjectives)
+  n2 = random.choice(nouns)
+  n3 = random.choice(nouns)
+  n4 = random.choice(nouns)
   np1 = random.choice(nounphrase)
   np2 = random.choice(nounphrase)
-  vb = random.choice(verbs)
-  print(np1 + " " +  vb  + " " + a1  + " " + np2)
+  np3 = random.choice(nounphrase)
+  vb1 = random.choice(verbs)
+  vb2 = random.choice(verbs)
+  t = random.choice(there)
+  pv1 = random.choice(presverb)
+  pv2 = random.choice(presverb)
+  pv3 = random.choice(presverb)
+  pl1 = random.choice(plnoun)
+  pl2 = random.choice(plnoun)
+  pl3 = random.choice(plnoun)
+  pn1 = random.choice(pronoun)
+  pn2 = random.choice(pronoun)
+  g1 = random.choice(gerund)
+  g2 = random.choice(gerund)
+  g3 = random.choice(gerund)
 
-for i in range(1):
-  a1 = random.choice(adjectives)
-  np1 = random.choice(nounphrase)
-  vb = random.choice(verbs)
-  print(vb  + " " + a1  + " " + np1)
-  
-for i in range(1):
-  vb = random.choice(verbs)
-  print(vb)
+  print('~' + a3 + " " + pl1 + "~")
+  print (a1 + " " + np1)
+  print (pn1 + " " + pv1 + " " + a2 + " " + n1)
+  print ('there are ' + a3 + " " + pl1 + " " + g1 + " " + n2 + " ")
+  print (a5 + " " + np2 + " " + pv2 + " " + vb1)
+  print (pl2 + " " + g2 + " " + np3)
+  print ('there are ' + a4 + " " + pl3 + " " + g3 + " " + n3 + " ")
+  print (pn2 + " " + pv3 + " " + a6 + " " + n4)
+
